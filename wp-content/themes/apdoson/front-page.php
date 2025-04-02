1b91e3e9b03d119fe481de1d0d297a3ab98f9bd4
Donz <donnygapulao@gmail.com>
2025-04-03 01:47:55 +0800

    typo

--- wp-content/themes/apdoson/front-page.php
+++ wp-content/themes/apdoson/front-page.php
@@ -83,7 +83,7 @@
               <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-logistics-blue flex-shrink-0 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
-              <span class="text-gray-700">10+ years of industry expertise</span>
+              <span class="text-gray-700">5+ years of industry expertise</span>
             </div>
             <div class="flex items-start space-x-3 fade-element fade-delay-400">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-logistics-blue flex-shrink-0 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
