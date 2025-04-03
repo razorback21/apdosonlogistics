c35d2f2fca7d905180832095a182f9aea5dde46b
Donz <donnygapulao@gmail.com>
2025-04-03 16:10:04 +0800

    lolgo

--- wp-content/themes/apdoson/page-contact.php
+++ wp-content/themes/apdoson/page-contact.php
@@ -46,12 +46,20 @@
                     <h3 class="font-semibold text-logistics-dark">Phone</h3>
                     <p class="text-gray-600">(02) 8525-5779</p>
                   </div>
-                  <div>
+                  <div  class="mb-2">
                     <h3 class="font-semibold text-logistics-dark">Mobile No.</h3>
                     <p class="text-gray-600">
                       SMART: +63 963-101-4747 | +63 920-903-5779 <br/>
                       GLOBE: +63 956-423-1844 | +63 950-696-1845 <br/>
                       WHATSAPP: +956-423-1844
+                    </p>
+                  </div>
+                  <div>
+                    <h3 class="font-semibold text-logistics-dark">Viber</h3>
+                    <p class="text-gray-600">
+                    (0956) 423 1844 </br>
+                    (0963) 1014747 <br/>
+                    (0928) 469 0277
                     </p>
                   </div>
                 </div>
@@ -91,16 +99,16 @@
             <div class="mt-12 fade-element fade-delay-200">
               <h3 class="text-xl font-bold mb-6 text-logistics-dark">Follow Us</h3>
               <div class="flex space-x-4">
-                <a href="#" class="p-2 bg-logistics-blue/10 rounded-lg text-logistics-blue hover:bg-logistics-blue hover:text-white transition-colors duration-300">
+                <a href="https://www.facebook.com/ApdosonPH/" target="_blank" class="p-2 bg-logistics-blue/10 rounded-lg text-logistics-blue hover:bg-logistics-blue hover:text-white transition-colors duration-300">
                   <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                     <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
                   </svg>
                 </a>
-                <a href="#" class="p-2 bg-logistics-blue/10 rounded-lg text-logistics-blue hover:bg-logistics-blue hover:text-white transition-colors duration-300">
+                <a href="https://www.youtube.com/channel/UCaKnK-p-72ZXgnwCY9fJdrg/featured" target="_blank" class="p-2 bg-logistics-blue/10 rounded-lg text-logistics-blue hover:bg-logistics-blue hover:text-white transition-colors duration-300">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-youtube-icon lucide-youtube"><path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"/><path d="m10 15 5-3-5-3z"/></svg>
                 </a>
                 
-                <a href="#" class="p-2 bg-logistics-blue/10 rounded-lg text-logistics-blue hover:bg-logistics-blue hover:text-white transition-colors duration-300">
+                <a href="https://www.instagram.com/apdosonphilippines/" target="_blank" class="p-2 bg-logistics-blue/10 rounded-lg text-logistics-blue hover:bg-logistics-blue hover:text-white transition-colors duration-300">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
                   </svg>
