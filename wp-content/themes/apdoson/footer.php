baf75cc2be2d9e49fa8efbcd549addd1bf794cf7
Donz <donnygapulao@gmail.com>
2025-04-03 16:15:51 +0800

    sched

--- wp-content/themes/apdoson/footer.php
+++ wp-content/themes/apdoson/footer.php
@@ -153,7 +153,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-logistics-blue mt-0.5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
-                <span class="text-gray-600">Mon - Fri: 8:00 AM - 5:00 PM<br>Sat: 8:00 AM - 12:00 PM</span>
+                <span class="text-gray-600">Mon - Fri: 8:00 AM - 5:00 PM<br>Sat: 8:00 AM - 5:00 PM</span>
               </li>
             </ul>
           </div>
